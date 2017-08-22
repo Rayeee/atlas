6521472083ad4bdd8595d68457786ffc5db3cd61
zhugongyi <zhugongyi@niwodai.net>
2017-08-22 22:05:48 +0800

    修改base controller 的返回

--- atlas-web/src/main/java/com/eparty/ccp/web/framework/WebAPIBaseController.java
+++ atlas-web/src/main/java/com/eparty/ccp/web/framework/WebAPIBaseController.java
@@ -1,9 +1,8 @@
 package com.eparty.ccp.web.framework;
 
-import com.eparty.ccp.contract.exception.ServiceException;
-import com.eparty.ccp.contract.exception.SystemException;
+import com.joindata.inf.common.basic.exceptions.ServiceException;
+import com.joindata.inf.common.basic.exceptions.SystemException;
 import com.joindata.inf.common.util.log.Logger;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -17,15 +16,15 @@ public abstract class WebAPIBaseController {
         if (e instanceof ServiceException || e.getCause() instanceof ServiceException) {
             ServiceException serviceException = (ServiceException) (e instanceof ServiceException ? e : e.getCause());
             logger.error("request请求【业务异常】code = {},msg = {}", serviceException.getCode(), serviceException.getMessage());
-            if (StringUtils.isEmpty(serviceException.getCode())) {
+            if (serviceException.getCode() == 0) {
                 return RestResponse.fail(serviceException.getMessage());
             } else {
-                return RestResponse.fail(serviceException.getCode(), serviceException.getMessage());
+                return RestResponse.fail(serviceException.getCode() + "", serviceException.getMessage());
             }
         } else if (e instanceof SystemException || e.getCause() instanceof SystemException) {
             SystemException systemException = (SystemException) (e instanceof SystemException ? e : e.getCause());
             logger.error("request请求【系统异常】code = {},msg = {}", systemException.getCode(), systemException.getMessage());
-            return RestResponse.fail(systemException.getCode(), "系统异常，请稍后重试");
+            return RestResponse.fail(systemException.getCode() + "", "系统异常，请稍后重试");
         } else {
             logger.error("request请求【未知异常】", e);
             return RestResponse.fail("系统异常，请稍后重试");
